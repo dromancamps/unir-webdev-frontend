4c0bd2c8742e93a0563949c9f7b2fadb49a16ecf
David Roman <dromancamps@gmail.com>
2023-05-13 00:03:35 +0200

    data

--- src/router/MovieRouter.js
+++ src/router/MovieRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { RentsView } from "../views/RentsView"
 import { MediaView}  from "../views/MediaView"
 import { ExplorerView } from "../views/ExplorerView"
